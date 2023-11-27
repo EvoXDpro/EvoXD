be09e39c884351098b133d327df0d89e94239f29
EvoXDpro <152088627+EvoXDpro@users.noreply.github.com>
2023-11-28 02:55:46 +0530

    Update utils.py
    Signed-off-by: EvoXDpro <152088627+EvoXDpro@users.noreply.github.com>

--- pyPuii/startup/utils.py
+++ pyPuii/startup/utils.py
@@ -32,12 +32,12 @@ def load_addons(plugin_name):
     base_name = plugin_name.split("/")[-1].split("\\")[-1].replace(".py", "")
     if base_name.startswith("__"):
         return
-    from pyPuii import fns
+    from pyEvoXD import fns
 
-    from .. import HNDLR, LOGS, asst, udB, puii_bot
+    from .. import HNDLR, LOGS, asst, udB, EvoXD_bot
     from .._misc import _supporter as config
     from .._misc._assistant import asst_cmd, callback, in_pattern
-    from .._misc._decorators import puii_cmd
+    from .._misc._decorators import EvoXD_cmd
     from .._misc._supporter import Config, admin_cmd, sudo_cmd
     from .._misc._wrappers import eod, eor
     from ..configs import Var
@@ -48,19 +48,19 @@ def load_addons(plugin_name):
     mod = util.module_from_spec(spec)
     for path in configPaths:
         modules[path] = config
-    modules["pyPuii.functions"] = fns
+    modules["pyEvoXD.functions"] = fns
     mod.LOG_CHANNEL = udB.get_key("LOG_CHANNEL")
     mod.udB = udB
     mod.asst = asst
     mod.tgbot = asst
-    mod.puii_bot = puii_bot
-    mod.ub = puii_bot
-    mod.bot = puii_bot
-    mod.puii = puii_bot
-    mod.borg = puii_bot
-    mod.telebot = puii_bot
-    mod.jarvis = puii_bot
-    mod.friday = puii_bot
+    mod.EvoXD_bot = EvoXD_bot
+    mod.ub = EvoXD_bot
+    mod.bot = EvoXD_bot
+    mod.EvoXD = EvoXD_bot
+    mod.borg = EvoXD_bot
+    mod.telebot = EvoXD_bot
+    mod.jarvis = EvoXD_bot
+    mod.friday = EvoXD_bot
     mod.eod = eod
     mod.edit_delete = eod
     mod.LOGS = LOGS
@@ -74,8 +74,8 @@ def load_addons(plugin_name):
     mod.eor = eor
     mod.edit_or_reply = eor
     mod.asst_cmd = asst_cmd
-    mod.puii_cmd = puii_cmd
-    mod.on_cmd = puii_cmd
+    mod.EvoXD_cmd = EvoXD_cmd
+    mod.on_cmd = EvoXD_cmd
     mod.callback = callback
     mod.Redis = udB.get_key
     mod.admin_cmd = admin_cmd
