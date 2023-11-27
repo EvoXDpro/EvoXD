b217da99fe74433f5a3f78c334f22c74799c7021
EvoXDpro <152088627+EvoXDpro@users.noreply.github.com>
2023-11-28 00:50:14 +0530

    Update connections.py
    Signed-off-by: EvoXDpro <152088627+EvoXDpro@users.noreply.github.com>

--- pyPuii/startup/connections.py
+++ pyPuii/startup/connections.py
@@ -15,7 +15,7 @@ from telethon.sessions.string import _STRUCT_PREFORMAT, CURRENT_VERSION, StringS
 
 from ..configs import Var
 from . import *
-from .BaseClient import PuiiClient
+from .BaseClient import EvoXDClient
 
 _PYRO_FORM = {351: ">B?256sI?", 356: ">B?256sQ?", 362: ">BI?256sQ?"}
 
@@ -73,14 +73,14 @@ def validate_session(session, logger=LOGS, _exit=True):
         sys.exit()
 
 
-def vc_connection(udB, Puii_bot):
+def vc_connection(udB, EvoXD_bot):
     from strings import get_string
 
     VC_SESSION = Var.VC_SESSION or udB.get_key("VC_SESSION")
     if VC_SESSION and VC_SESSION != Var.SESSION:
         LOGS.info("Starting up VcClient.")
         try:
-            return PuiiClient(
+            return EvoXDClient(
                 validate_session(VC_SESSION, _exit=False),
                 log_attempt=False,
                 exit_on_error=False,
@@ -91,4 +91,4 @@ def vc_connection(udB, Puii_bot):
         except Exception as er:
             LOGS.info("While creating Client for VC.")
             LOGS.exception(er)
-    return Puii_bot
+    return EvoXD_bot
