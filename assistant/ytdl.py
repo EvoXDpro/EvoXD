cc30996e5d056d580b7600e2a884ee4812cd53f1
EvoXDpro <152088627+EvoXDpro@users.noreply.github.com>
2023-11-28 22:59:13 +0530

    Update ytdl.py
    Signed-off-by: EvoXDpro <152088627+EvoXDpro@users.noreply.github.com>

--- assistant/ytdl.py
+++ assistant/ytdl.py
@@ -12,14 +12,14 @@ from telethon.errors.rpcerrorlist import FilePartLengthInvalidError, MediaEmptyE
 from telethon.tl.types import DocumentAttributeAudio, DocumentAttributeVideo
 from telethon.tl.types import InputWebDocument as wb
 
-from pyPuii.fns.helper import (
+from pyEvoXD.fns.helper import (
     bash,
     fast_download,
     humanbytes,
     numerize,
     time_formatter,
 )
-from pyPuii.fns.ytdl import dler, get_buttons, get_formats
+from pyEvoXD.fns.ytdl import dler, get_buttons, get_formats
 
 from . import LOGS, asst, callback, in_pattern, udB
 
