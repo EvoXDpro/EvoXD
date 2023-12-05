3dd7dc5f33c474f376a0baaba4c6a814c08527b7
EvoXDpro <152088627+EvoXDpro@users.noreply.github.com>
2023-12-05 23:20:26 +0530

    Update calculator.py
    Signed-off-by: EvoXDpro <152088627+EvoXDpro@users.noreply.github.com>

--- plugins/calculator.py
+++ plugins/calculator.py
@@ -12,7 +12,7 @@ __doc__ = get_help("help_calculator")
 
 import re
 
-from . import Button, asst, callback, get_string, in_pattern, udB, puii_cmd
+from . import Button, asst, callback, get_string, in_pattern, udB, EvoXD_cmd
 
 CALC = {}
 
@@ -43,7 +43,7 @@ lst = list(zip(tultd[::4], tultd[1::4], tultd[2::4], tultd[3::4]))
 lst.append([Button.inline("=", data="calc=")])
 
 
-@puii_cmd(pattern="calc")
+@EvoXD_cmd(pattern="calc")
 async def icalc(e):
     udB.del_key("calc")
     if e.client._bot:
